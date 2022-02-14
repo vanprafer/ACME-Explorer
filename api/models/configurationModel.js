977e7440bdad5d96db28d28bb849e2bf71bfa622
vanprafer <vanprafer@alum.us.es>
2022-02-14 20:06:30 +0100

    Modified some comments and another sintaxis

--- api/models/configurationModel.js
+++ api/models/configurationModel.js
@@ -1,30 +1,30 @@
-"use strict";
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+'use strict'
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const ConfigurationSchema = new Schema(
   {
     flatRate: {
       type: Number,
-      required: "Kindly enter the value of the flat rate",
+      required: 'Kindly enter the value of the flat rate'
     },
     finderCache: {
       type: Number,
       default: 1,
       min: 1,
-      max: 24,
+      max: 24
     },
     finderResults: {
       type: Number,
       default: 10,
-      max: 100,
+      max: 100
     },
     created: {
       type: Date,
-      default: Date.now,
-    },
+      default: Date.now
+    }
   },
   { strict: false }
-);
+)
 
-module.exports = mongoose.model("Configuration", ConfigurationSchema);
+module.exports = mongoose.model('Configuration', ConfigurationSchema)
--- api/routes/configurationRoutes.js
+++ api/routes/configurationRoutes.js
@@ -1,26 +1,26 @@
-"use strict";
+'use strict'
 module.exports = function (app) {
-  const configurations = require("../controllers/configurationController");
+  const configurations = require('../controllers/configurationController')
 
   app
-    .route("/v0/configurations")
+    .route('/v0/configurations')
     .get(configurations.list_all_configurations_v0)
-    .post(configurations.create_a_configuration_v0);
+    .post(configurations.create_a_configuration_v0)
 
   app
-    .route("/v1/configurations")
+    .route('/v1/configurations')
     .get(configurations.list_all_configurations)
-    .post(configurations.create_a_configuration);
+    .post(configurations.create_a_configuration)
 
   app
-    .route("/v0/configurations/:configurationId")
+    .route('/v0/configurations/:configurationId')
     .get(configurations.read_a_configuration)
     .put(configurations.update_a_configuration_v0)
-    .delete(configurations.delete_a_configuration_v0);
+    .delete(configurations.delete_a_configuration_v0)
 
   app
-    .route("/v1/configurations/:configurationId")
+    .route('/v1/configurations/:configurationId')
     .get(configurations.read_a_configuration)
     .put(configurations.update_a_configuration)
-    .delete(configurations.delete_a_configuration);
-};
+    .delete(configurations.delete_a_configuration)
+}
