346c9228559ada0f4f9e207d53e41dc002db0d67
Luis <luisgallego778@gmail.com>
2022-03-01 19:43:55 +0100

    Removed _v0 and simplify configurations crud

--- api/controllers/actorController.js
+++ api/controllers/actorController.js
@@ -3,16 +3,6 @@
 const mongoose = require('mongoose')
 const Actor = mongoose.model('Actors')
 
-exports.list_all_actors_v0 = function (req, res) {
-  Actor.find({}, function (err, actors) {
-    if (err) {
-      res.status(500).send(err)
-    } else {
-      res.json(actors)
-    }
-  })
-}
-
 exports.list_all_actors = function (req, res) {
   // Check if the role param exist
   /*
@@ -26,17 +16,6 @@ exports.list_all_actors = function (req, res) {
       res.status(500).send(err)
     } else {
       res.json(actors)
-    }
-  })
-}
-
-exports.create_an_actor_v0 = function (req, res) {
-  const newActor = new Actor(req.body)
-  newActor.save(function (err, actor) {
-    if (err) {
-      res.status(500).send(err)
-    } else {
-      res.json(actor)
     }
   })
 }
@@ -57,16 +36,6 @@ exports.create_an_actor = function (req, res) {
   })
 }
 
-exports.read_an_actor_v0 = function (req, res) {
-  Actor.findById(req.params.actorId, function (err, actor) {
-    if (err) {
-      res.status(500).send(err)
-    } else {
-      res.json(actor)
-    }
-  })
-}
-
 exports.read_an_actor = function (req, res) {
   // Check that the user is an Administrator or proper actor and if not: res.status(403);
   // "an access token is valid, but requires more privileges"
@@ -79,7 +48,9 @@ exports.read_an_actor = function (req, res) {
   })
 }
 
-exports.update_an_actor_v0 = function (req, res) {
+exports.update_an_actor = function (req, res) {
+  // Check that the user is the proper actor and if not: res.status(403);
+  // "an access token is valid, but requires more privileges"
   Actor.findOneAndUpdate(
     { _id: req.params.actorId },
     req.body,
@@ -94,39 +65,6 @@ exports.update_an_actor_v0 = function (req, res) {
   )
 }
 
-exports.update_an_actor = function (req, res) {
-  // Check that the user is the proper actor and if not: res.status(403);
-  // "an access token is valid, but requires more privileges"
-  Actor.findOneAndUpdate(
-    { _id: req.params.actorId },
-    req.body,
-    { new: true },
-    function (err, actor) {
-      if (err) {
-        res.status(500).send(err)
-      } else {
-        res.json(actor)
-      }
-    }
-  )
-}
-
-exports.unban_an_actor_v0 = function (req, res) {
-  console.log('Unban an actor with id: ' + req.params.actorId)
-  Actor.findOneAndUpdate(
-    { _id: req.params.actorId },
-    { $set: { banned: false } },
-    { new: true },
-    function (err, actor) {
-      if (err) {
-        res.status(500).send(err)
-      } else {
-        res.json(actor)
-      }
-    }
-  )
-}
-
 exports.unban_an_actor = function (req, res) {
   // Check that the user is an Administrator and if not: res.status(403);
   // "an access token is valid, but requires more privileges"
@@ -134,22 +72,6 @@ exports.unban_an_actor = function (req, res) {
   Actor.findOneAndUpdate(
     { _id: req.params.actorId },
     { $set: { banned: false } },
-    { new: true },
-    function (err, actor) {
-      if (err) {
-        res.status(500).send(err)
-      } else {
-        res.json(actor)
-      }
-    }
-  )
-}
-
-exports.ban_an_actor_v0 = function (req, res) {
-  console.log('Ban an actor with id: ' + req.params.actorId)
-  Actor.findOneAndUpdate(
-    { _id: req.params.actorId },
-    { $set: { banned: true } },
     { new: true },
     function (err, actor) {
       if (err) {
@@ -179,7 +101,7 @@ exports.ban_an_actor = function (req, res) {
   )
 }
 
-exports.delete_an_actor_v0 = function (req, res) {
+exports.delete_an_actor = function (req, res) {
   Actor.deleteOne({ _id: req.params.actorId }, function (err, actor) {
     if (err) {
       res.status(500).send(err)
