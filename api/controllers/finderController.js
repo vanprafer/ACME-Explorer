430853539c902f04c9cd983ce458da82bb866a73
Luis <luisgallego778@gmail.com>
2022-03-10 13:28:43 +0100

    finder create

--- api/controllers/finderController.js
+++ api/controllers/finderController.js
@@ -1,114 +1,103 @@
-'use strict'
+"use strict";
 
-const mongoose = require('mongoose')
-const Finder = mongoose.model('Finders')
-
-exports.list_all_finders_v0 = function (req, res) {
-  Finder.find({}, function (err, finders) {
-    if (err) {
-      res.send(err)
-    } else {
-      res.json(finders)
-    }
-  })
-}
+const mongoose = require("mongoose");
+const Finder = mongoose.model("Finders");
+const configurations = require("../controllers/configurationController");
+const moment = require("moment");
 
 exports.list_all_finders = function (req, res) {
   Finder.find({}, function (err, finders) {
     if (err) {
-      res.status(500).send(err)
+      res.status(500).send(err);
     } else {
-      res.json(finders)
+      res.json(finders);
     }
-  })
-}
-
-exports.create_a_finder_v0 = function (req, res) {
-  const newFinder = new Finder(req.body)
-  newFinder.save(function (err, finder) {
-    if (err) {
-      res.send(err)
-    } else {
-      res.json(finder)
-    }
-  })
-}
+  });
+};
 
 exports.create_a_finder = function (req, res) {
-  const newFinder = new Finder(req.body)
-  newFinder.save(function (err, finder) {
-    if (err) {
-      if (err.name === 'ValidationError') {
-        res.status(422).send(err)
-      } else {
-        res.status(500).send(err)
-      }
-    } else {
-      res.json(finder)
-    }
-  })
-}
+  const newFinder = new Finder(req.body);
+  const finderCache = configurations.get_configuration()["finderCache"];
+  const cacheDate = moment().subtract(finderCache, "hours").toDate();
 
-exports.read_a_finder_v0 = function (req, res) {
-  Finder.findById(req.params.finderId, function (err, finder) {
-    if (err) {
-      res.send(err)
-    } else {
-      res.json(finder)
-    }
+  Finder.find({
+    keyword: req.params.keyword,
+    maxPrice: req.params.maxPrice,
+    startDate: req.params.startDate,
+    endDate: req.params.endDate,
+    cachedDataDate: { $gt: cacheDate },
   })
-}
+    .limit(1)
+    .exec({}, function (err, finders) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        if (finders.length > 0) {
+          read_a_finder(finders[0]._id);
+        } else {
+          newFinder.save(function (err, finder) {
+            if (err) {
+              if (err.name === "ValidationError") {
+                res.status(422).send(err);
+              } else {
+                res.status(500).send(err);
+              }
+            } else {
+              res.json(finder);
+            }
+          });
+        }
+      }
+    });
+};
+
+exports.list_my_finders = function (req, res) {
+  // Check whether the logged user is a Explorer
+  // Return finders owned by the Explorer
+  Trip.find({ explorer: req.paramas._id }, function (err, trips) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(trips);
+    }
+  });
+};
 
 exports.read_a_finder = function (req, res) {
   Finder.findById(req.params.finderId, function (err, finder) {
     if (err) {
-      res.status(500).send(err)
+      res.status(500).send(err);
     } else {
-      res.json(finder)
+      res.json(finder);
     }
-  })
-}
-
-exports.update_a_finder_v0 = function (req, res) {
-  Finder.findOneAndUpdate({ _id: req.params.finderId }, req.body, { new: true }, function (err, finder) {
-    if (err) {
-      res.send(err)
-    } else {
-      res.json(finder)
-    }
-  })
-}
+  });
+};
 
 exports.update_a_finder = function (req, res) {
-  Finder.findOneAndUpdate({ _id: req.params.finderId }, req.body, { new: true }, function (err, finder) {
-    if (err) {
-      if (err.name === 'ValidationError') {
-        res.status(422).send(err)
+  Finder.findOneAndUpdate(
+    { _id: req.params.finderId },
+    req.body,
+    { new: true },
+    function (err, finder) {
+      if (err) {
+        if (err.name === "ValidationError") {
+          res.status(422).send(err);
+        } else {
+          res.status(500).send(err);
+        }
       } else {
-        res.status(500).send(err)
+        res.json(finder);
       }
-    } else {
-      res.json(finder)
     }
-  })
-}
-
-exports.delete_a_finder_v0 = function (req, res) {
-  Finder.deleteOne({ _id: req.params.finderId }, function (err, finder) {
-    if (err) {
-      res.send(err)
-    } else {
-      res.json({ message: 'Finder successfully deleted' })
-    }
-  })
-}
+  );
+};
 
 exports.delete_a_finder = function (req, res) {
   Finder.deleteOne({ _id: req.params.finderId }, function (err, finder) {
     if (err) {
-      res.status(500).send(err)
+      res.status(500).send(err);
     } else {
-      res.json({ message: 'Finder successfully deleted' })
+      res.json({ message: "Finder successfully deleted" });
     }
-  })
-}
+  });
+};
--- api/routes/finderRoutes.js
+++ api/routes/finderRoutes.js
@@ -12,13 +12,9 @@ module.exports = function (app) {
    * @type get post
    * @url /v1/finders/
   */
-  app.route('/v1/finders')
+  app.route('/v0/finders')
     // .get(finders.read_my_finder)
     .post(finders.create_a_finder)
-
-  app.route('/v0/finders')
-  // .get(finders.read_my_finder_v0)
-    .post(finders.create_a_finder_v0)
 
   /**
    * Modify a finder
@@ -28,9 +24,7 @@ module.exports = function (app) {
    * @type put
    * @url /v1/finders/:finderId
   */
-  app.route('/v1/finders/:finderId')
+  app.route('/v0/finders/:finderId')
     .put(finders.update_a_finder)
 
-  app.route('/v0/finders/:finderId')
-    .put(finders.update_a_finder_v0)
 }
