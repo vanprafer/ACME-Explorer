27c0a0a2b36e9b0bac1f5c24ce3f5a0411ddc87c
vanprafer <vanprafer@alum.us.es>
2022-03-19 17:25:58 +0100

    Created endpoints for verified roles and deleted list_applications. Updated thunder and routes too.

--- api/controllers/applicationController.js
+++ api/controllers/applicationController.js
@@ -3,28 +3,76 @@
 const mongoose = require('mongoose')
 const Application = mongoose.model('Applications')
 const Trip = mongoose.model('Trips')
+const Actor = mongoose.model('Actors')
+const authController = require('./authController')
 
-exports.list_all_applications = function (req, res) {
-  Application.find({}, function (err, applications) {
-    if (err) {
-      res.status(500).send(err)
-    } else {
-      res.json(applications)
-    }
+exports.list_my_applications = function (req, res) {
+  const idToken = req.headers.idtoken
+
+  authController.getUserId(idToken).then(function (loggedUser) {
+    Actor.findById(loggedUser, function (err, actor) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        if (actor) {
+          if (actor.roles.includes('EXPLORER')) {
+            Application.aggregate([
+              { $match: { actor: loggedUser } },
+              { $group: { status: 1 } }
+            ], function (err, applications) {
+              if (err) {
+                res.send(err)
+              } else {
+                res.json(applications)
+              }
+            })
+          } else {
+            res.status(405).send('The Actor has unidentified roles') // Not allowed
+          }
+        } else {
+          res.status(404).send('Cannot find actor')
+        }
+      }
+    })
   })
 }
 
-exports.list_my_applications = function (req, res) {
-  const loggedUser = '' // Saco el id de la persona logueada
-  Application.aggregate([
-    { $match: { actor: loggedUser } },
-    { $group: { status: 1 } }
-  ], function (err, applications) {
-    if (err) {
-      res.send(err)
-    } else {
-      res.json(applications)
-    }
+exports.list_my_applications_by_trip = function (req, res) {
+  const trip = req.params.tripId
+  const idToken = req.headers.idtoken
+
+  authController.getUserId(idToken).then(function (loggedUser) {
+    Actor.findById(loggedUser, function (err, actor) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        if (actor) {
+          if (actor.roles.includes('MANAGER')) {
+            Trip.findById(trip, function (err, trip) {
+              if (err) {
+                res.status(500).send(err)
+              } else {
+                if (trip && trip.manager === loggedUser) {
+                  Application.find({ trip: trip }, function (err, applications) {
+                    if (err) {
+                      res.status(500).send(err)
+                    } else {
+                      res.json(applications)
+                    }
+                  })
+                } else {
+                  res.status(405).send('The Actor has unidentified roles') // Not allowed
+                }
+              }
+            })
+          } else {
+            res.status(405).send('The Actor has unidentified roles') // Not allowed
+          }
+        } else {
+          res.status(404).send('Cannot find actor')
+        }
+      }
+    })
   })
 }
 
@@ -64,6 +112,59 @@ exports.create_an_application = function (req, res) {
   })
 }
 
+exports.create_an_application_verified = function (req, res) {
+  const idToken = req.headers.idtoken
+  const newApplication = new Application(req.body)
+
+  authController.getUserId(idToken).then(function (loggedUser) {
+    Actor.findById(loggedUser, function (err, actor) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        if (actor) {
+          if (actor.roles.includes('EXPLORER')) {
+            Trip.findById(newApplication.trip, function (err, trip) {
+              if (err) {
+                res.status(500).send(err)
+              } else {
+                if (trip) {
+                  const startDate = trip.dateStart
+                  const cancelationReason = trip.cancelationReason
+
+                  if (Date.now() >= startDate) {
+                    res.status(500).send('The trip you are applying for has already begun')
+                  } else if (cancelationReason) {
+                    res.status(500).send('The trip you are applying for has been cancelled')
+                  } else {
+                    newApplication.status = 'PENDING'
+                    newApplication.save(function (err, application) {
+                      if (err) {
+                        if (err.name === 'ValidationError') {
+                          res.status(422).send(err)
+                        } else {
+                          res.status(500).send(err)
+                        }
+                      } else {
+                        res.json(application)
+                      }
+                    })
+                  }
+                } else {
+                  res.status(404).send('Trip not found')
+                }
+              }
+            })
+          } else {
+            res.status(405).send('The Actor has unidentified roles') // Not allowed
+          }
+        } else {
+          res.status(404).send('Cannot find actor')
+        }
+      }
+    })
+  })
+}
+
 exports.read_an_application = function (req, res) {
   Application.findById(req.params.applicationId, function (err, application) {
     if (err) {
@@ -71,6 +172,54 @@ exports.read_an_application = function (req, res) {
     } else {
       res.json(application)
     }
+  })
+}
+
+exports.read_an_application_verified = function (req, res) {
+  const idToken = req.headers.idtoken
+
+  authController.getUserId(idToken).then(function (loggedUser) {
+    Actor.findById(loggedUser, function (err, actor) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        if (actor) {
+          if (actor.roles.includes('EXPLORER')) {
+            Application.findById(req.params.applicationId, function (err, application) {
+              if (err) {
+                res.status(500).send(err)
+              } else if (application.explorer !== loggedUser) {
+                res.status(405).send('The Actor has unidentified roles') // Not allowed
+              } else {
+                res.json(application)
+              }
+            })
+          } else if (actor.roles.includes('MANAGER')) {
+            Application.findById(req.params.applicationId, function (err, application) {
+              if (err) {
+                res.status(500).send(err)
+              } else {
+                Trip.findById(application.trip, function (err, trip) {
+                  if (err) {
+                    res.status(500).send(err)
+                  } else {
+                    if (trip && trip.manager === loggedUser) {
+                      res.json(application)
+                    } else {
+                      res.status(405).send('The Actor has unidentified roles') // Not allowed
+                    }
+                  }
+                })
+              }
+            })
+          } else {
+            res.status(405).send('The Actor has unidentified roles') // Not allowed
+          }
+        } else {
+          res.status(404).send('Cannot find actor')
+        }
+      }
+    })
   })
 }
 
@@ -86,6 +235,76 @@ exports.update_an_application = function (req, res) {
       application.status = req.params.status
       res.json(application)
     }
+  })
+}
+
+exports.update_an_application_verified = function (req, res) {
+  const idToken = req.headers.idtoken
+
+  authController.getUserId(idToken).then(function (loggedUser) {
+    Actor.findById(loggedUser, function (err, actor) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        if (actor) {
+          if (actor.roles.includes('EXPLORER')) {
+            Application.findById(req.params.applicationId, function (err, application) {
+              if (err) {
+                res.status(500).send(err)
+              } else if (application.explorer !== loggedUser) {
+                res.status(405).send('The Actor has unidentified roles') // Not allowed
+              } else {
+                Application.findOneAndUpdate({ _id: req.params.applicationId }, req.body, { new: true }, function (err, application) {
+                  if (err) {
+                    if (err.name === 'ValidationError') {
+                      res.status(422).send(err)
+                    } else {
+                      res.status(500).send(err)
+                    }
+                  } else {
+                    application.status = req.params.status
+                    res.json(application)
+                  }
+                })
+              }
+            })
+          } else if (actor.roles.includes('MANAGER')) {
+            Application.findById(req.params.applicationId, function (err, application) {
+              if (err) {
+                res.status(500).send(err)
+              } else {
+                Trip.findById(application.trip, function (err, trip) {
+                  if (err) {
+                    res.status(500).send(err)
+                  } else {
+                    if (trip && trip.manager === loggedUser) {
+                      Application.findOneAndUpdate({ _id: req.params.applicationId }, req.body, { new: true }, function (err, application) {
+                        if (err) {
+                          if (err.name === 'ValidationError') {
+                            res.status(422).send(err)
+                          } else {
+                            res.status(500).send(err)
+                          }
+                        } else {
+                          application.status = req.params.status
+                          res.json(application)
+                        }
+                      })
+                    } else {
+                      res.status(405).send('The Actor has unidentified roles') // Not allowed
+                    }
+                  }
+                })
+              }
+            })
+          } else {
+            res.status(405).send('The Actor has unidentified roles') // Not allowed
+          }
+        } else {
+          res.status(404).send('Cannot find actor')
+        }
+      }
+    })
   })
 }
 
@@ -105,6 +324,56 @@ exports.reject_an_application = function (req, res) {
   )
 }
 
+exports.reject_an_application_verified = function (req, res) {
+  console.log('Reject an application with id: ' + req.params.applicationId)
+  const idToken = req.headers.idtoken
+
+  authController.getUserId(idToken).then(function (loggedUser) {
+    Actor.findById(loggedUser, function (err, actor) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        if (actor) {
+          if (actor.roles.includes('MANAGER')) {
+            Application.findById(req.params.applicationId, function (err, application) {
+              if (err) {
+                res.status(500).send(err)
+              } else {
+                Trip.findById(application.trip, function (err, trip) {
+                  if (err) {
+                    res.status(500).send(err)
+                  } else {
+                    if (trip && trip.manager === loggedUser) {
+                      Application.findOneAndUpdate(
+                        { _id: req.params.applicationId },
+                        { $set: { status: 'REJECTED' } },
+                        { new: true },
+                        function (err, application) {
+                          if (err) {
+                            res.status(500).send(err)
+                          } else {
+                            res.json(application)
+                          }
+                        }
+                      )
+                    } else {
+                      res.status(405).send('The Actor has unidentified roles') // Not allowed
+                    }
+                  }
+                })
+              }
+            })
+          } else {
+            res.status(405).send('The Actor has unidentified roles') // Not allowed
+          }
+        } else {
+          res.status(404).send('Cannot find actor')
+        }
+      }
+    })
+  })
+}
+
 exports.due_an_application = function (req, res) {
   console.log('Cancel an application with id: ' + req.params.applicationId)
   Application.findOneAndUpdate(
@@ -119,6 +388,56 @@ exports.due_an_application = function (req, res) {
       }
     }
   )
+}
+
+exports.due_an_application_verified = function (req, res) {
+  console.log('Due an application with id: ' + req.params.applicationId)
+  const idToken = req.headers.idtoken
+
+  authController.getUserId(idToken).then(function (loggedUser) {
+    Actor.findById(loggedUser, function (err, actor) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        if (actor) {
+          if (actor.roles.includes('MANAGER')) {
+            Application.findById(req.params.applicationId, function (err, application) {
+              if (err) {
+                res.status(500).send(err)
+              } else {
+                Trip.findById(application.trip, function (err, trip) {
+                  if (err) {
+                    res.status(500).send(err)
+                  } else {
+                    if (trip && trip.manager === loggedUser) {
+                      Application.findOneAndUpdate(
+                        { _id: req.params.applicationId },
+                        { $set: { status: 'DUE' } },
+                        { new: true },
+                        function (err, application) {
+                          if (err) {
+                            res.status(500).send(err)
+                          } else {
+                            res.json(application)
+                          }
+                        }
+                      )
+                    } else {
+                      res.status(405).send('The Actor has unidentified roles') // Not allowed
+                    }
+                  }
+                })
+              }
+            })
+          } else {
+            res.status(405).send('The Actor has unidentified roles') // Not allowed
+          }
+        } else {
+          res.status(404).send('Cannot find actor')
+        }
+      }
+    })
+  })
 }
 
 exports.accept_an_application = function (req, res) {
@@ -147,6 +466,50 @@ exports.accept_an_application = function (req, res) {
   })
 }
 
+exports.accept_an_application_verified = function (req, res) {
+  console.log('Accept an application with id: ' + req.params.applicationId)
+  const idToken = req.headers.idtoken
+
+  authController.getUserId(idToken).then(function (loggedUser) {
+    Actor.findById(loggedUser, function (err, actor) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        if (actor) {
+          if (actor.roles.includes('EXPLORER')) {
+            Application.findById(req.params.applicationId, function (err, application) {
+              if (err) {
+                res.status(500).send(err)
+              } else if (application.explorer !== loggedUser) {
+                res.status(500).send('Unable to accept this application')
+              } else if (application.status !== 'DUE') {
+                res.status(500).send('Unable to accept this application')
+              } else {
+                Application.findOneAndUpdate(
+                  { _id: req.params.applicationId },
+                  { $set: { status: 'ACCEPTED' } },
+                  { new: true },
+                  function (err, application) {
+                    if (err) {
+                      res.status(500).send(err)
+                    } else {
+                      res.json(application)
+                    }
+                  }
+                )
+              }
+            })
+          } else {
+            res.status(405).send('The Actor has unidentified roles') // Not allowed
+          }
+        } else {
+          res.status(404).send('Cannot find actor')
+        }
+      }
+    })
+  })
+}
+
 exports.cancel_an_application = function (req, res) {
   console.log('Cancel an application with id: ' + req.params.applicationId)
   Application.findById(req.params.applicationId, function (err, application) {
@@ -170,5 +533,49 @@ exports.cancel_an_application = function (req, res) {
         )
       }
     }
+  })
+}
+
+exports.cancel_an_application_verified = function (req, res) {
+  console.log('Cancel an application with id: ' + req.params.applicationId)
+  const idToken = req.headers.idtoken
+
+  authController.getUserId(idToken).then(function (loggedUser) {
+    Actor.findById(loggedUser, function (err, actor) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        if (actor) {
+          if (actor.roles.includes('EXPLORER')) {
+            Application.findById(req.params.applicationId, function (err, application) {
+              if (err) {
+                res.status(500).send(err)
+              } else if (application.explorer !== loggedUser) {
+                res.status(500).send('Unable to cancel this application')
+              } else if (application.status !== 'PENDING' && application.status !== 'ACCEPTED') {
+                res.status(500).send('Unable to cancel this application')
+              } else {
+                Application.findOneAndUpdate(
+                  { _id: req.params.applicationId },
+                  { $set: { status: 'CANCELLED' } },
+                  { new: true },
+                  function (err, application) {
+                    if (err) {
+                      res.status(500).send(err)
+                    } else {
+                      res.json(application)
+                    }
+                  }
+                )
+              }
+            })
+          } else {
+            res.status(405).send('The Actor has unidentified roles') // Not allowed
+          }
+        } else {
+          res.status(404).send('Cannot find actor')
+        }
+      }
+    })
   })
 }
--- api/routes/applicationRoutes.js
+++ api/routes/applicationRoutes.js
@@ -8,24 +8,43 @@ module.exports = function (app) {
    *
    * @section my_applications
    * @type get
-   * @url /v0/my_applications
+   * @url /v1/my_applications
   */
-  app.route('/v0/my_applications')
+  app.route('/v1/my_applications')
     .get(applications.list_my_applications)
 
   /**
-   * Get applications
+   * Get my applications - Grouped by Status
    *    RequiredRoles: Manager
+   *
+   * @section my_applications
+   * @type get
+   * @url /v1/my_applications
+  */
+  app.route('/v1/my_applications/:tripId')
+    .get(applications.list_my_applications_by_trip)
+
+  /**
    * Create an application
    *    RequiredRoles: Explorer
    *
    * @section applications
-   * @type get post
+   * @type post
    * @url /v0/applications
   */
   app.route('/v0/applications')
-    .get(applications.list_all_applications)
     .post(applications.create_an_application)
+
+  /**
+   * Create an application
+   *    RequiredRoles: Explorer
+   *
+   * @section applications
+   * @type post
+   * @url /v1/applications
+  */
+  app.route('/v1/applications')
+    .post(applications.create_an_application_verified)
 
   /**
    * Get an application
@@ -44,12 +63,36 @@ module.exports = function (app) {
     .put(applications.update_an_application)
 
   /**
+   * Get an application
+   *    RequiredRoles: Explorer, Manager
+   *
+   * Modify an application
+   *    RequiredRoles: Explorer, Manager
+   *
+   * @section applications
+   * @type get put
+   * @url /v1/applications/:applicationId
+   * @param {string} applicationId
+  */
+  app.route('/v1/applications/:applicationId')
+    .get(applications.read_an_application_verified)
+    .put(applications.update_an_application_verified)
+
+  /**
    * Reject an application
    *    RequiredRoles: Manager
    * @param {string} applicationId
   */
   app.route('/v0/applications/:applicationId/reject')
     .patch(applications.reject_an_application)
+
+  /**
+   * Reject an application
+   *    RequiredRoles: Manager
+   * @param {string} applicationId
+  */
+  app.route('/v1/applications/:applicationId/reject')
+    .patch(applications.reject_an_application_verified)
 
   /**
    * Manager accepts an application (is DUE)
@@ -60,6 +103,14 @@ module.exports = function (app) {
     .patch(applications.due_an_application)
 
   /**
+   * Manager accepts an application (is DUE)
+   *    RequiredRoles: Manager
+   *    @param {string} applicationId
+  */
+  app.route('/v1/applications/:applicationId/due')
+    .patch(applications.due_an_application_verified)
+
+  /**
    * Accept an application which has been PAID by the Explorer
    *    RequiredRoles: Explorer
    *    @param {string} applicationId
@@ -68,10 +119,26 @@ module.exports = function (app) {
     .patch(applications.accept_an_application)
 
   /**
+   * Accept an application which has been PAID by the Explorer
+   *    RequiredRoles: Explorer
+   *    @param {string} applicationId
+  */
+  app.route('/v1/applications/:applicationId/accept')
+    .patch(applications.accept_an_application_verified)
+
+  /**
    * Cancel an application by the creator Explorer. It must have an status "PENDING" or "ACCEPTED"
    *    RequiredRoles: Explorer
    *    @param {string} applicationId
   */
   app.route('/v0/applications/:applicationId/cancel')
     .patch(applications.cancel_an_application)
+
+  /**
+   * Cancel an application by the creator Explorer. It must have an status "PENDING" or "ACCEPTED"
+   *    RequiredRoles: Explorer
+   *    @param {string} applicationId
+  */
+  app.route('/v1/applications/:applicationId/cancel')
+    .patch(applications.cancel_an_application_verified)
 }
